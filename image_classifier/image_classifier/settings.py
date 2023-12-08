1c81521ec5381058d0fabfb02effe42bd1bbb639
unknown <ayushpoddar586@gmail.com>
2023-12-09 03:12:55 +0530

    updating according to deploy

--- image_classifier/image_classifier/settings.py
+++ image_classifier/image_classifier/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gin93danv1xykb^um9n*3*c&!z-htj_6u2alu4x5ps+kr(_bv2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost','*']
 
 
 # Application definition
@@ -77,7 +77,11 @@ WSGI_APPLICATION = 'image_classifier.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD':'DbG3FbBF*G5ffAFAdEBG*A5gDA5b-Da4',
+        'HOST':'viaduct.proxy.rlwy.net',
+        'PORT':'20912',
     }
 }
 
@@ -117,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static') 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
